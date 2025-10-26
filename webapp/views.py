3d9f5ca24d3d49ce19f951c263219d7ca9ea6f47
Asel Chubashova <asel619918@gmail.com>
2025-10-26 21:47:03 +0600

    Создала страницу список статей

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from webapp.models import Article
 
-# Create your views here.
+
+def article_list_view(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'article_list.html', context)
